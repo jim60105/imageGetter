3546f227a09ced7682d3951be78ca0222df9c49d
jim60105 <jim60105@gmail.com>
2016-06-14 02:36:12 +0800

    add: open the original pic in the new tab at the background change: button words

--- content_script.js
+++ content_script.js
@@ -6,7 +6,7 @@ function checkForValidUrl(url) {
     if(getDomainFromUrl(url).toLowerCase()=="chan.sankakucomplex.com"){
         //添加按鈕
         var newBar = $('<div style="text-align: center"><div class="bar"></div></div>')
-        var saveBtn = $('<input type="button" value="GetOriginalLink" style="padding: 0.2em 1em"/>').click(save);
+        var saveBtn = $('<input type="button" value="OpenOriginalPic" style="padding: 0.2em 1em"/>').click(save);
         var saveA = $('<a href="#" style="padding: 0.4em 1em;display:none">DirectUrl</a>');
 
         $(".content div span").append(newBar);
@@ -36,7 +36,18 @@ function save() {
             //將取得結果解析至網頁中
             var img=$(PageContent).find("a#highres")[0].href;
             a.href = img;
+            openNewBackgroundTab(img);
             a.style.display = "";
             btn.style.display = "none";
     });
+}
+
+function openNewBackgroundTab(url){
+    var a = document.createElement("a");
+    a.href = url; 
+    var evt = document.createEvent("MouseEvents");
+    //the tenth parameter of initMouseEvent sets ctrl key
+    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+                       true, false, false, false, 0, null);
+    a.dispatchEvent(evt);
 }
